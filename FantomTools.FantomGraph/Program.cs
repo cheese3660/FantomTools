afb2e5edfdf15b3e958c19df77dc6dff30f2c099
pearl <pearl@rrr.sh>
2024-09-24 20:02:00 -0400

    add generation of jump/switch tracking stuff to FantomGraph

--- FantomTools.FantomGraph/Program.cs
+++ FantomTools.FantomGraph/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text;
 using FantomTools.Fantom;
+using FantomTools.Fantom.Code.Instructions;
+using FantomTools.Fantom.Code.Operations;
 
 var sb = new StringBuilder();
 sb.AppendLine("flowchart TD");
@@ -25,19 +27,42 @@ foreach (var type in pod.Types.Where(type => types.Contains(type.Name)))
         {
             sb.AppendLine($"            {methodPrefix}_{label.Value}[{label.Value}]");
         }
+
+        sb.AppendLine();
         
         // iterate over the instructions in the method body, tracking which label we're under
         var curLabel = "start";
         foreach (var instr in method.Body.Instructions)
         {
             // figure out if our current label changed
-            var possibleNewLabels = 
-            if (newLabel != null)
+            var possibleNewLabels = sortedLabels
+                .Where(l => l.Key > instr.Offset)
+                .ToList();
+            if (possibleNewLabels.Count != 0)
+                curLabel = possibleNewLabels.FirstOrDefault().Value;
+
+            switch (instr)
             {
+                case JumpInstruction jumpInstruction:
+                {
+                    var destLabel = sortedLabels[jumpInstruction.Target.Offset];
+                    var opcodeLabel = Operations.OperationsByType[jumpInstruction.OpCode].Name;
                 
+                    sb.AppendLine($"        {methodPrefix}_{curLabel} -- {opcodeLabel} --> {methodPrefix}_{destLabel}");
+                    break;
+                }
+                case SwitchInstruction switchInstruction:
+                {
+                    var opcodeLabel = Operations.OperationsByType[switchInstruction.OpCode].Name;
+                    foreach (var target in switchInstruction.JumpTargets)
+                    {
+                        var destLabel = sortedLabels[target.Offset];
+                        sb.AppendLine(
+                            $"            {methodPrefix}_{curLabel} -- {opcodeLabel} --> {methodPrefix}_{destLabel}");
+                    }
+                    break;
+                }
             }
-                
-            if ()
         }
         
         sb.AppendLine("        end");
